6b2143fd056c2c57743db90e225497aaff632caf
Shubham Bhilare <140585267+Skb3611@users.noreply.github.com>
2024-11-13 08:37:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -192,8 +192,10 @@ export default function LandingPage() {
                 <div className="flex flex-col items-center space-y-2 text-center">
                   <Phone className="h-10 w-10 mb-2 text-primary" />
                   <h3 className="text-xl font-bold">Phone</h3>
-                  <a href="tel:+1234567890" className="text-gray-500 dark:text-gray-400 hover:underline">
-                    +91 7775858902 / +91 78881 24630
+                  <a href="tel:+91 7775858902" className="text-gray-500 dark:text-gray-400 hover:underline">
+                    +91 7775858902 </a> /
+<a href="tel:+91 78881 24630" className="text-gray-500 dark:text-gray-400 hover:underline">
+                     +91 78881 24630
                   </a>
                 </div>
               </div>
